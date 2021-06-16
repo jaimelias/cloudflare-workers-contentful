4fdd9fef0ff537b3899b07ced19c01b55d6e54a7
jaimelias <escribeme@jaimelias.com>
2021-06-16 12:08:41 -0500

    fix logo size issue in smaller devices

--- src/hooks/scripts/bootstrapScripts.js
+++ src/hooks/scripts/bootstrapScripts.js
@@ -10,7 +10,7 @@ export const bootstrapScripts = theme => {
 				order: 100,
 				inline: [{
 					location: 'after',
-					content: `body,html{height:100vh}.container{max-width:1200px}#footer a:not(.btn){color:#444}.entry-content img{max-width:100%;height:auto}.hidden{display:none}.chat-component{z-index:9999}hr{margin: 3rem 0}.entry-content p, .entry-content h1, .entry-content h2, .entry-content h3, .entry-content h4, .entry-content table, .entry-content iframe, .row:not(.form-row), .entry-content ul, .entry-content ol, .entry-content blockquote, .entry-content code{margin-bottom: 1.5rem!important} .entry-content ul > li, .entry-content ol > li{margin-bottom: 0.5rem!important},`
+					content: `body,html{height:100vh}.container{max-width:1200px}#footer a:not(.btn){color:#444}.entry-content img{max-width:100%;height:auto}.hidden{display:none}.chat-component{z-index:9999}hr{margin: 3rem 0}.entry-content p, .entry-content h1, .entry-content h2, .entry-content h3, .entry-content h4, .entry-content table, .entry-content iframe, .row:not(.form-row), .entry-content ul, .entry-content ol, .entry-content blockquote, .entry-content code{margin-bottom: 1.5rem!important} .entry-content ul > li, .entry-content ol > li{margin-bottom: 0.5rem!important}@media(max-width: 576px){.navbar-brand > img {max-width: 200px}}`
 				}]
 			},
 			bootstrapScriptsJs: {
